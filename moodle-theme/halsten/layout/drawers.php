0cc3b4e5099f21223f610885a926108066dcf08e
{amandacostah} <{amanda.costa@halsten.com.br}>
2026-04-30 14:36:17 -0300

    fix: corrige drawers.php compatibilidade Moodle 4.5

--- moodle-theme/halsten/layout/drawers.php
+++ moodle-theme/halsten/layout/drawers.php
@@ -4,15 +4,12 @@ defined('MOODLE_INTERNAL') || die();
 $bodyattributes = $OUTPUT->body_attributes([]);
 $blockshtml = $OUTPUT->blocks('side-pre');
 $hasblocks = strpos($blockshtml, 'data-block=') !== false;
-$buildregionmainsettings = !$PAGE->include_region_main_settings_in_header_actions() && !$PAGE->has_blocks();
 $regionmainsettingsmenu = $OUTPUT->region_main_settings_menu();
-$header = $OUTPUT->full_header();
-$sidepreblocks = $OUTPUT->blocks('side-pre');
 
 $templatecontext = [
     'sitename' => format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID)]),
     'output' => $OUTPUT,
-    'sidepreblocks' => $sidepreblocks,
+    'sidepreblocks' => $blockshtml,
     'hasblocks' => $hasblocks,
     'bodyattributes' => $bodyattributes,
     'regionmainsettingsmenu' => $regionmainsettingsmenu,
