e3111eab095e569cd88a1c4511db1b06abd7e7e2
{amandacostah} <{amanda.costa@halsten.com.br}>
2026-04-30 13:54:44 -0300

    fix: adiciona arquivos obrigatorios do tema

---
+++ moodle-theme/halsten/lang/en/theme_halsten.php
@@ -0,0 +1,3 @@
+<?php
+$string['pluginname'] = 'Halsten Academy';
+$string['choosereadme'] = 'Tema oficial da Halsten Academy.';
---
+++ moodle-theme/halsten/version.php
@@ -0,0 +1,6 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2025043000;
+$plugin->requires = 2024100700;
+$plugin->component = 'theme_halsten';
