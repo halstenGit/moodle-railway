139e600ebcb1eed4330341ea0c36a09a7ad4cb6f
{amandacostah} <{amanda.costa@halsten.com.br}>
2026-05-06 14:47:59 -0300

    fix: persiste config.php no volume entre deploys

--- moodle-theme/halsten/settings.php
+++ moodle-theme/halsten/settings.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 if ($ADMIN->fulltree) {
-    $settings = new theme_boost_admin_settingspage_tabs('theme_halsten', get_string('configtitle', 'theme_boost'));
-    $page = new admin_settingpage('theme_halsten_general', get_string('generalsettings', 'theme_boost'));
-    $settings->add($page);
+    $setting = new admin_setting_heading('theme_halsten/generalheading',
+        get_string('pluginname', 'theme_halsten'), '');
+    $settings->add($setting);
 }
